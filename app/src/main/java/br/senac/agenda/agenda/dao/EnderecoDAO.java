3990a51fe18c49e88626d1a3d57b02fffa309d0a
JoaoVBalduino <joaovb200.jv@gmail.com>
2019-02-18 22:14:27 -0300

    Comeco da criacao do Endereco e Models att 2

--- app/src/main/java/br/senac/agenda/agenda/dao/EnderecoDAO.java
+++ app/src/main/java/br/senac/agenda/agenda/dao/EnderecoDAO.java
@@ -3,13 +3,18 @@ package br.senac.agenda.agenda.dao;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
+import br.senac.agenda.agenda.model.ContatoEntity;
 import br.senac.agenda.agenda.model.EnderecoEntity;
 
 public class EnderecoDAO {
     private SQLiteHelper sqLiteHelper;
     private SQLiteDatabase sqLiteDatabase;
 
-   public EnderecoDAO(Context context){this.sqLiteHelper = new SQLiteHelper(context);}
+    public EnderecoDAO(Context context) {
+        this.sqLiteHelper = new SQLiteHelper(context);
+    }
 
-   
+    public void salvar(EnderecoEntity contato) {
+        sqLiteDatabase = sqLiteHelper.getWritableDatabase();
+    }
 }
